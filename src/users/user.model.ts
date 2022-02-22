1572e49dc52c2c7e7075e0c2f9d1722bfdde9e86
dekelbyz <bayazi455@gmail.com>
2022-02-22 20:11:56 +0200

    add comments and change syntax issues

--- src/users/user.model.ts
+++ src/users/user.model.ts
@@ -1,10 +1,14 @@
 import * as mongoose from 'mongoose';
 
+/** Represents our User Schema for mongoose */
 export const UserSchema = new mongoose.Schema({
   username: { type: String, required: true },
   password: { type: String, required: true },
 });
 
+/** Represents our User stracture as it appears
+ * in our database.
+ */
 export interface User {
   id: string;
   username: string;
